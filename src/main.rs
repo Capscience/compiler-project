9de160fdd59e9d9e9e3a8d3679c309ebcf6fb8ea
Simo Soini <caps@foobar.fi>
2024-01-19 17:56:11 +0200

    Added temporary testing code to main function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::path::Path;
+
+use compiler_project::compile;
+
 fn main() {
-    println!("Hello, world!");
+    let source_code: &'static str = "
+    // This is placeholder stuff for testing tokenization.
+    fn testfunc(a/* inline comment */) {
+        if a > 10 then print_int(a);
+        // comment
+        b = a;
+        /* this
+            is
+            multiline comment */
+        return b;
+    }";
+    compile(source_code, Path::new("executable"));
 }
