16c05d79b4b513893a9702c91bdd3e14392bfed0
Simo Soini <caps@foobar.fi>
2024-01-19 17:55:04 +0200

    Added function that will take care of the whole compilation process.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,16 @@
+use std::path::Path;
+
+use tokenizer::tokenize;
+
 /// Tokenizer and support struct module.
 pub mod tokenizer;
+
+/// Incomplete function handling the complete compilation process.
+pub fn compile(source_code: &'static str, _output: &Path) {
+    let tokens = tokenize(source_code);
+    if cfg!(debug_assertions) {
+        for token in tokens {
+            print!("{}, ", token);
+        }
+    }
+}
