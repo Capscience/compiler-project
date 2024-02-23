058416b0f3e5f709765ccc74b31824e81a8caba7
Simo Soini <simo@capscience.fi>
2024-02-23 19:55:54 +0200

    Added new steps in generating IR.

--- src/ir_generator.rs
+++ src/ir_generator.rs
@@ -1,20 +1,80 @@
 use std::collections::HashMap;
 
 use crate::{
-    ast::{Expr, Type},
+    ast::{Expr, ExprKind, Type},
     ir::{IRVar, Instruction},
     variable::SymbolTable,
 };
 
-pub fn generate_ir(root_types: HashMap<String, Type>, exprs: Vec<Expr>) {
-    let mut var_types = root_types.clone();
-    let mut var_number = 1;
-    let new_var = |t: Type| {
-        let name = format!("x{var_number}");
-        var_number += 1;
-        var_types.insert(name.clone(), t);
-        name
-    };
+pub struct IRGenerator {
+    var_number: i32,
+    var_types: HashMap<String, Type>,
+    var_none: String,
+    instructions: Vec<Instruction>,
+}
 
-    let mut instructions: Vec<Instruction> = Vec::new();
+impl IRGenerator {
+    pub fn new(mut var_types: HashMap<String, Type>) -> Self {
+        let var_none = "none".to_string();
+        var_types.insert(var_none.clone(), Type::None);
+        Self {
+            var_number: 1,
+            var_types,
+            var_none,
+            instructions: Vec::new(),
+        }
+    }
+
+    pub fn new_var(&mut self, t: Type) -> IRVar {
+        let name = format!("x{}", self.var_number);
+        self.var_number += 1;
+        self.var_types.insert(name.clone(), t);
+        name
+    }
+
+    pub fn visit(&mut self, symbol_table: &mut SymbolTable<IRVar>, expr: Expr) -> IRVar {
+        let mut var = String::new();
+        match expr.content {
+            ExprKind::Literal { value } => match expr.type_ {
+                Type::Int => {
+                    var = self.new_var(Type::Int);
+                    self.instructions.push(Instruction::LoadIntConst {
+                        value: value.parse::<i64>().expect("Checked by typechecker."),
+                        dest: var.clone(),
+                    });
+                }
+                Type::Bool => {
+                    var = self.new_var(Type::Bool);
+                    self.instructions.push(Instruction::LoadBoolConst {
+                        value: value.parse::<bool>().expect("Checked by typechecker."),
+                        dest: var.clone(),
+                    });
+                }
+                Type::None => {
+                    var = self.var_none.clone();
+                }
+            },
+            ExprKind::Identifier { value } => {
+                var = symbol_table
+                    .get(&value)
+                    .expect("Type checker should have caught this missing value")
+                    .to_string();
+            }
+            _ => todo!(),
+        };
+        var
+    }
+}
+
+pub fn generate_ir(root_types: HashMap<String, Type>, exprs: Vec<Expr>) {
+    let mut generator = IRGenerator::new(root_types.clone());
+
+    let mut root_symtab: SymbolTable<String> = SymbolTable::new(None);
+    for key in root_types.keys() {
+        root_symtab.declare(key.to_string()).unwrap();
+        root_symtab.set(key.to_string(), key.to_string()).unwrap();
+    }
+    for expr in exprs {
+        generator.visit(&mut root_symtab, expr);
+    }
 }
