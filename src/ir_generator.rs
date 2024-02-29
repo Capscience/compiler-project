99ea603f4ed87594f63f84599fe2d82239a0b704
Simo Soini <caps@foobar.fi>
2024-02-29 16:00:20 +0200

    Implemented rest of the IR generator.

--- src/ir_generator.rs
+++ src/ir_generator.rs
@@ -8,6 +8,7 @@ use crate::{
 
 pub struct IRGenerator {
     var_number: i32,
+    label_number: i32,
     var_types: HashMap<String, Type>,
     var_none: String,
     instructions: Vec<Instruction>,
@@ -19,17 +20,28 @@ impl IRGenerator {
         var_types.insert(var_none.clone(), Type::None);
         Self {
             var_number: 1,
+            label_number: 1,
             var_types,
             var_none,
             instructions: Vec::new(),
         }
     }
 
-    pub fn new_var(&mut self, t: Type) -> IRVar {
+    fn new_var(&mut self, t: Type) -> IRVar {
         let name = format!("x{}", self.var_number);
         self.var_number += 1;
         self.var_types.insert(name.clone(), t);
         name
+    }
+
+    fn new_label(&mut self) -> IRVar {
+        let name = format!("L{}", self.label_number);
+        self.label_number += 1;
+        name
+    }
+
+    fn emit(&mut self, instruction: Instruction) {
+        self.instructions.push(instruction);
     }
 
     pub fn visit(&mut self, symbol_table: &mut SymbolTable<IRVar>, expr: Expr) -> IRVar {
@@ -38,14 +50,14 @@ impl IRGenerator {
             ExprKind::Literal { value } => match expr.type_ {
                 Type::Int => {
                     var = self.new_var(Type::Int);
-                    self.instructions.push(Instruction::LoadIntConst {
+                    self.emit(Instruction::LoadIntConst {
                         value: value.parse::<i64>().expect("Checked by typechecker."),
                         dest: var.clone(),
                     });
                 }
                 Type::Bool => {
                     var = self.new_var(Type::Bool);
-                    self.instructions.push(Instruction::LoadBoolConst {
+                    self.emit(Instruction::LoadBoolConst {
                         value: value.parse::<bool>().expect("Checked by typechecker."),
                         dest: var.clone(),
                     });
@@ -60,7 +72,86 @@ impl IRGenerator {
                     .expect("Type checker should have caught this missing value")
                     .to_string();
             }
-            _ => todo!(),
+            ExprKind::BinaryOperation {
+                left,
+                operation,
+                right,
+            } => {
+                let var_left = self.visit(symbol_table, *left);
+                let var_right = self.visit(symbol_table, *right);
+                if &operation == "=" {
+                    self.emit(Instruction::Copy {
+                        source: var_right.clone(),
+                        dest: var_left.clone(),
+                    });
+                    var = var_left;
+                } else {
+                    let var_result = self.new_var(expr.type_.clone());
+                    self.emit(Instruction::Call {
+                        fun: operation,
+                        args: vec![var_left, var_right],
+                        dest: var_result.clone(),
+                    });
+                    var = var_result;
+                }
+            }
+            ExprKind::VarDeclaration { identifier } => {
+                let _ = symbol_table.declare(identifier.clone());
+                let _ = symbol_table.set(identifier.clone(), self.new_var(Type::None));
+                var = symbol_table
+                    .get(&identifier)
+                    .expect("This should never happen.")
+                    .to_string();
+                // TODO: proper error handling
+            }
+            ExprKind::IfClause {
+                condition,
+                if_block,
+                else_block,
+            } => {
+                if let Some(else_block) = else_block {
+                    let l_then = self.new_label();
+                    let l_else = self.new_label();
+                    let l_end = self.new_label();
+
+                    let var_cond = self.visit(symbol_table, *condition);
+
+                    self.emit(Instruction::CondJump {
+                        cond: var_cond,
+                        then_label: l_then.clone(),
+                        else_label: l_else.clone(),
+                    });
+                    self.emit(Instruction::Label { name: l_then });
+                    self.visit(symbol_table, *if_block);
+                    self.emit(Instruction::Jump {
+                        label: l_end.clone(),
+                    });
+                    self.emit(Instruction::Label { name: l_else });
+                    self.visit(symbol_table, *else_block);
+                    self.emit(Instruction::Label { name: l_end });
+                    var = self.new_var(expr.type_.clone());
+                } else {
+                    let l_then = self.new_label();
+                    let l_end = self.new_label();
+
+                    let var_cond = self.visit(symbol_table, *condition);
+
+                    self.emit(Instruction::CondJump {
+                        cond: var_cond,
+                        then_label: l_then.clone(),
+                        else_label: l_end.clone(),
+                    });
+                    self.emit(Instruction::Label { name: l_then });
+                    self.visit(symbol_table, *if_block);
+                    self.emit(Instruction::Label { name: l_end });
+                    var = self.var_none.clone();
+                }
+            } // _ => todo!(),
+            ExprKind::Block { expressions } => {
+                for expr in expressions {
+                    var = self.visit(symbol_table, expr);
+                }
+            }
         };
         var
     }
