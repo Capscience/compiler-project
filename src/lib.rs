6cc15c1bf5f1494c3f4b921fc2aa0a60cb38fc19
Simo Soini <simo@capscience.fi>
2024-03-05 17:11:52 +0200

    Made compile function do the whole compilation.

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,31 @@ pub mod tokenizer;
 pub mod type_checker;
 pub mod variable;
 
+use std::collections::HashMap;
 use std::path::Path;
 
+use assembler::assemble;
+use assembly_generator::generate_assembly;
+use ir_generator::generate_ir;
+use parser::parse;
 use tokenizer::tokenize;
+use type_checker::TypeChecker;
 
-/// Incomplete function handling the complete compilation process.
-pub fn compile(source_code: &'static str, _output: &Path) {
-    let tokens = tokenize(source_code);
-    if cfg!(debug_assertions) {
-        for token in tokens {
-            print!("{}, ", token);
+pub fn compile(code: String, output: &Path) {
+    let mut typechecker = TypeChecker::new();
+    let ast_result = parse(&tokenize(&code));
+    if let Err(error) = ast_result {
+        println!("Parsing error: {error}");
+        std::process::exit(1);
+    }
+    let mut ast = ast_result.expect("Handled above.");
+    for mut node in &mut ast {
+        if let Err(error) = typechecker.typecheck(&mut node) {
+            println!("Typecheck error: {error}");
+            std::process::exit(1);
         }
     }
+    let instructions = generate_ir(HashMap::new(), ast);
+    let assembly = generate_assembly(&instructions);
+    assemble(assembly, output)
 }
